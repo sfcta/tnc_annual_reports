0bcfb20a366295cd00c200c960dd3295edf64905
Drew Cooper <Drew@sfcta.org>
2023-07-26 14:45:27 -0700

    Add logic to account for field name errors
    Signed-off-by: Drew Cooper <Drew@sfcta.org>

--- utils.py
+++ utils.py
@@ -2,11 +2,29 @@ import pandas as pd
 
 def find_name_match(name, candidates):
     for c in candidates:
-        if str(name).lower().strip().replace('_','').replace(' ','') == str(c).lower().strip().replace('_','').replace(' ',''):
+        simp_name = str(name).lower().strip().replace('_','').replace(' ','').replace(r'ï»¿','')
+        simp_c = str(c).lower().strip().replace('_','').replace(' ','')
+        
+        simp_c = simp_c.replace('harrass','harass')
+        
+        if simp_c == 'totalmilesmt':
+            simp_c = 'totalmilesmth'
+        if simp_c == 'meanmilesmt':
+            simp_c = 'meanmilesmth'
+        if simp_c == 'medianmilesmt':
+            simp_c = 'medianmlesmth'
+        if simp_c == 'carrierid':
+            simp_c = 'tncid'
+        
+        if ((simp_name == simp_c) or 
+            (simp_name.replace('hours','miles') == simp_c) or 
+            (simp_name.replace('miles','hours') == simp_c) or
+            (simp_name.replace('assaut','assault') == simp_c)):
             if str(name) == str(c):
                 return (c, 'exact')
             else:
                 return (c, 'approx')
+                
     return (None, None)
     
 def fix_field_names(report, data_dict):
@@ -15,7 +33,6 @@ def fix_field_names(report, data_dict):
         m, t = find_name_match(c, data_dict.index.tolist())
         if m != None:
             rename[c] = m
-      
     out = report.rename(columns=rename)
     return out
     
